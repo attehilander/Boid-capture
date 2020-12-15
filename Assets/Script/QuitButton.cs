cff0f6a0e7eaa34bef734dbb003d1c8aa200e776
Mikko Kyllönen <mikkokyllonen1@kamk.fi>
2020-12-15 21:26:33 +0200

    Added quit button

---
+++ Assets/Script/QuitButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class QuitButton : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
