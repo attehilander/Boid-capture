1fd0685355782e23f00febf7e24e39b3b35bf04d
attehil <attehilander@kamk.fi>
2020-12-15 14:21:23 +0200

    failed coding

--- Assets/Script/FlockBehavior.cs
+++ Assets/Script/FlockBehavior.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class FlockBehavior : ScriptableObject
+
 {
     public abstract Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock);
+   
 }
